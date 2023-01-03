d69dfe3c720f4b1a91241b9fedfa308f4c111b95
Alan <alantinoco@gmail.com>
2023-01-02 21:36:41 -0300

    Add static files

--- agenda/views.py
+++ agenda/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'agendamento.html')
 
+
+def tabelaDePrecos(request):
+    return render(request, 'tabelaDePrecos.html')

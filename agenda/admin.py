0e189fdc6701f3ea1bbfb015d68804442d965b7b
Alan <alantinoco@gmail.com>
2023-01-03 11:24:14 -0300

    add models

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Barbeiro, Serviço, Agendamento
 
-# Register your models here.
+
+admin.site.register(Barbeiro)
+admin.site.register(Serviço)
+admin.site.register(Agendamento)
--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Barbeiro(models.Model):
+    nomeBarbeiro = models.CharField(max_length=50)
+    telBarbeiro = models.CharField(max_length=11)
+    estaAtivo = models.BooleanField()
+
+    def __str__(self):
+        return self.nomeBarbeiro
+
+class Serviço(models.Model):
+    nomeServico = models.CharField(max_length=30)
+    precoServico = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return str(self.serviceName) + ": $" + str(self.servicePrice)
+
+
+class Agendamento(models.Model):
+    nomeCliente = models.CharField(max_length=50)
+    telCliente = models.CharField(max_length=11)
+    barbeiro = models.ForeignKey(Barbeiro, on_delete=models.CASCADE)
+    servico = models.ForeignKey(Serviço, null=True, blank=True, on_delete=models.CASCADE)
+    dataAgendamento = models.DateField()
+    horaAgendamento = models.CharField(max_length=20)
+    compareceu = models.BooleanField(null=True, blank=True)
+    observacoes = models.TextField(null=True, blank=True, max_length=240)
+
+    def __str__(self):
+        return self.clientName + '' + self.barber + '' + self.date
